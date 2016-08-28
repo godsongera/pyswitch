bf62ad6380044f971a42a88c784e14518e2e5743
Godson Gera <godson@lintelindia.com>
2016-08-28 16:10:34 +0530

    Removed MAX_LENGTH restriction to allow reading large messages generated by webRTC - reported by Silviu Caragea

---
+++ doc/pydoc/builddocs.py
@@ -0,0 +1,25 @@
+"""Script to automatically generate PyTable documentation"""
+import pydoc2
+
+if __name__ == "__main__":
+	excludes = [
+		"Numeric",
+		"_tkinter",
+		"Tkinter",
+		"math",
+		"string",
+		"twisted",
+	]
+	stops = [
+	]
+
+	modules = [
+		'pyswitch',		
+	]	
+	pydoc2.PackageDocumentationGenerator(
+		baseModules = modules,
+		destinationDirectory = ".",
+		exclusions = excludes,
+		recursionStops = stops,
+	).process ()
+	
